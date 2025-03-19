36037558086723fe528824093e0c084ae6d3c9e3
Vin-doong <vindoong06@gmail.com>
2025-03-19 12:38:13 +0900

    프론트엔드 올리기

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import MyPage from "./pages/MyPage";
+import ProductList from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
+import ReviewForm from "./pages/ReviewForm";
+import FavoriteList from "./pages/FavoriteList";
+import NoticeBoard from "./pages/NoticeBoard";
+import Schedule from "./pages/schedule";
+import NoticeBoardInsert from "./pages/NoticeBoardInsert";
+import { useCallback, useState } from "react";
+import NoticeBoardDetail from "./pages/NoticeBoardDetail";
+import NoticeBoardEdit from "./pages/NoticeBoardEdit";
+
+function App() {
+  //공지사항 관리 시 리덕스 등의 상태관리가 필요한데 리덕스 사용대신 App.jsx에서 넘겨주는 걸로 했습니다.
+  const [notices, setNotices] = useState([]);
+
+  const handleAddNotice = useCallback((newNotice) => {
+    setNotices((prevNotices) => [newNotice, ...prevNotices]);
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/review" element={<ReviewForm />} />
+        <Route path="/favorites" element={<FavoriteList />} />
+        <Route path="/notices" element={<NoticeBoard notices={notices}/>} />
+        <Route path="/notice/:id" element={<NoticeBoardDetail notices={notices} />} />
+        <Route path="/notices/edit/:id" element={<NoticeBoardEdit notices={notices} />} />
+        <Route path="/newnotice" element={<NoticeBoardInsert onSubmit={handleAddNotice} />} />
+        <Route path="/schedule" element={<Schedule />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/include/Header.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = ({ isTokenExpired }) => {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    const refreshToken = localStorage.getItem("refreshToken");
+    if (accessToken || refreshToken) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [isTokenExpired]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("check");
+    localStorage.removeItem("username");
+    localStorage.removeItem("email");
+    localStorage.removeItem("role");
+    localStorage.removeItem("id");
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <Navbar bg="white" expand="lg" className="shadow-sm">
+        <Container>
+          <Navbar.Brand href="/" className="text-teal-500 font-bold">SUPPLE IT</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link to="/" className="nav-link text-gray-700 hover:text-teal-500">홈</Link>
+              <Link to="/login" className='nav-link text-gray-700 hover:text-teal-500'>로그인</Link>
+              <Link to="/schedule" className='nav-link text-gray-700 hover:text-teal-500'>스케쥴</Link>
+              <Link to="/notice" className='nav-link text-gray-700 hover:text-teal-500'>공지사항</Link>
+            </Nav>
+            {isLoggedIn && (
+              <button 
+                type='button' 
+                onClick={handleLogout}
+                className="px-4 py-2 text-teal-500 border border-teal-500 hover:bg-teal-500 hover:text-white transition-colors rounded-button"
+              >
+                로그아웃
+              </button>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>    
+    </>
+  );
+};
+
+export default Header;
